5de6ec96678d2ff91142206607ddb9a7338747d2
AlvaroVazVel <43552181+AlvaroVazVel@users.noreply.github.com>
2020-03-04 20:29:43 -0600

    This is the codevision project

---
+++ OriginalProject.c
@@ -0,0 +1,17 @@
+/*
+ * OriginalProject.c
+ *
+ * Created: 04/03/2020 08:17:40 p. m.
+ * Author: vazqu
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
